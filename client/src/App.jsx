9e303ab5edf81b28007c70cea8340301f45a87b1
Janet Schindler <nic07_schindler@web.de>
2022-01-10 15:43:38 +0100

    changed package.json

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   useEffect(() => {
     const fetchMyHeadline = async () => {
-      const res = await fetch("http://localhost:7000/api");
+      const res = await fetch("/api");
       const data = await res.json();
       setMyHeadline(data.message);
     };

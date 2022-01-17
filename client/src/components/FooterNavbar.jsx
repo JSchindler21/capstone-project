42b25a0dc6e7267c5ebc02bf1c5eeecde80a9428
Janet Schindler <nic07_schindler@web.de>
2022-01-17 14:18:13 +0100

    build svg

--- client/src/components/FooterNavbar.jsx
+++ client/src/components/FooterNavbar.jsx
@@ -1,17 +1,37 @@
 import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+import HomeIcon from "../images/homepage.svg"
+import ExpenseTrackerIcon from "../images/expensetracker.svg"
+import SecretPlacesIcon from "../images/place.svg"
+import MyProfilIcon from "../images/profile.svg"
+import FavouritesIcon from "../images/earth+.svg"
+
 
 const FooterNavbar = () => {
   return (
     <div>
       <nav>
-        <NavLink to="home">Home</NavLink>
-        <NavLink to="expensetracker"> ExpenseTracker</NavLink>
-        <NavLink to="secretplaces">SecretPlaces</NavLink>
-        <NavLink to="myprofil">MyProfil</NavLink>
-        <NavLink to="favourites">Favourites</NavLink>
+        <NavLink to="home"> Home
+          <img scr={HomeIcon} />
+        </NavLink>
+        <NavLink to="expensetracker">
+        <img scr={ExpenseTrackerIcon} />
+        </NavLink>
+        <NavLink to="secretplaces">
+        <img scr={SecretPlacesIcon} />
+        </NavLink>
+        <NavLink to="myprofil">
+        <img scr={MyProfilIcon} />
+        </NavLink>
+        <NavLink to="favourites">
+        <img scr={FavouritesIcon} />
+        </NavLink>
       </nav>
     </div>
   );
 };
 
 export default FooterNavbar;
+
+

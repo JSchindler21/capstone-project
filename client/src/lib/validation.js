07ed0853ad1d90d45669ca48db11380dddd23801
Janet Schindler <nic07_schindler@web.de>
2022-02-03 18:20:53 +0100

    changed validation

--- client/src/lib/validation.js
+++ client/src/lib/validation.js
@@ -8,8 +8,8 @@ const hasValidurlImg = (urlImg) => urlImg !== '';
 
 const isTripValid = (trip) =>
 hasTripNameMinLength(trip.name, 3) &&
-hasTripNameMinLength(trip.country, 5) &&
-hasTripNameMinLength(trip.info, 20) &&
+hasTripNameMinLength(trip.country, 3) &&
+hasTripNameMinLength(trip.info, 3) &&
 hasValidCategory(trip.category) &&
 hasValidurlImg(trip.urlImg) &&
 hasValidurlImg(trip.imgUrl)
